a9ebc101b323ac32e9940dcecb8c0f5a52512e97
奕初 <hejianzhi@deepexi.com>
2024-02-08 17:34:48 +0800

    init

---
+++ src/router/routes/modules/evaluationFeedback.tsx
@@ -0,0 +1,55 @@
+import { Suspense, lazy } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { SvgIcon } from '@/components/icon';
+import { CircleLoading } from '@/components/loading';
+
+import { AppRouteObject } from '#/router';
+
+const IndicatorChangeApplicationPage = lazy(
+  () => import('@/pages/evaluation-feedback/evaluationIndexLibrary/indicatorChangeApplication'),
+);
+
+const management: AppRouteObject = {
+  order: 7,
+  path: 'evaluation-feedback',
+  element: (
+    <Suspense fallback={<CircleLoading />}>
+      <Outlet />
+    </Suspense>
+  ),
+  meta: {
+    label: 'sys.menu.evaluation',
+    icon: <SvgIcon icon="ic-management" className="ant-menu-item-icon" size="24" />,
+    key: '/evaluation-feedback',
+  },
+  children: [
+    {
+      index: true,
+      element: <Navigate to="evaluationIndexLibrary" replace />,
+    },
+    {
+      path: 'evaluationIndexLibrary',
+      meta: {
+        label: 'sys.menu.evaluation.evaluationIndexLibrary',
+        key: '/evaluation-feedback/evaluationIndexLibrary',
+      },
+      children: [
+        {
+          index: true,
+          element: <Navigate to="indicatorChangeApplication" replace />,
+        },
+        {
+          path: 'indicatorChangeApplication',
+          element: <IndicatorChangeApplicationPage />,
+          meta: {
+            label: 'sys.menu.user.indicatorChangeApplication',
+            key: '/management/evaluationIndexLibrary/indicatorChangeApplication',
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default management;
